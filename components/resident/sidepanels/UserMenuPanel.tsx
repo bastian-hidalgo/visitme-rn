8f0b4bea2f8153712150a4b22fdeb8926912a889
bastian-hidalgo <bastian_hidalgo@outlook.com>
2025-10-23 17:12:34 -0300

    remove overlay

--- components/resident/sidepanels/UserMenuPanel.tsx
+++ components/resident/sidepanels/UserMenuPanel.tsx
@@ -154,7 +154,6 @@ export default function UserMenuPanel({ isOpen, onClose }: Props) {
 const styles = StyleSheet.create({
   overlay: {
     flex: 1,
-    backgroundColor: 'rgba(255,255,255,0.2)',
     flexDirection: 'row',
     justifyContent: 'flex-end',
   },
